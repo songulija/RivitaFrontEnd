586bc7847fcc5857d5eff0ef18c097c8f7c8fe56
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-31 15:20:18 +0200

    register screen and header nav bar

---
+++ src/Screens/RegisterScreen.js
@@ -0,0 +1,72 @@
+import Button from '@restart/ui/esm/Button';
+import React from 'react'
+import { useState } from 'react';
+import { Form } from 'react-bootstrap'
+
+export const RegisterScreen = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    return (
+        <div>
+            <h1>Sign In</h1>
+
+            <Form>
+
+
+                <Form.Group controlId='text'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type='name'
+                        placeholder='Enter name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='Password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+
+                    </Form.Control>
+
+                </Form.Group>
+
+
+                <Form.Group controlId='Password'>
+                    <Form.Label>Confirm password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Confirm password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    >
+
+                    </Form.Control>
+
+                </Form.Group>
+
+                <Button type='Submit' variant='primary'> Sign In</Button>
+
+            </Form>
+        </div>
+    )
+}
