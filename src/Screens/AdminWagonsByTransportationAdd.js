0ef8413447c3ee14310477acb649c07690a954ac
songulija <65647664+songulija@users.noreply.github.com>
2021-11-07 14:52:10 +0200

    Added onDataChange and added checking original wagons and modified wagons states. Header appears if changed

--- src/Screens/AdminWagonsByTransportationAdd.js
+++ src/Screens/AdminWagonsByTransportationAdd.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'
-import { Modal, Button, Form, Space, Select, Input, Table,Row,Col,Card,Typography } from 'antd';
+import React from 'react'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Modal, Button, Form, Space, Select, Table, Row, Col, Card, Typography, InputNumber,Input } from 'antd';
 import { tableCardStyle, tableCardBodyStyle, buttonStyle } from '../styles/customStyles';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import { PlusOutlined } from '@ant-design/icons';
 import { getUserData } from '../redux/actions/userActions';
 import { getWagonsByTransportation, getlWagons, getWagons } from '../redux/actions/wagonsActions';
 import UnsavedChangesHeader from '../Component/UnsavedChangesHeader';
+import { useStateWithCallbackLazy } from 'use-state-with-callback';
 
 const aboutTitleTextStyle = {
     fontStyle: 'normal',
@@ -27,108 +28,211 @@ const textStyle = {
 
 const { Text } = Typography;
 
-function AdminWagonsByTransportationAdd(props) {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [wagons, setWagons] = useState([]);
-    const [transportationId, setTransportationId] = useState('');
-
-    //getting redux states
-    const usersReducer = useSelector((state) => state.usersReducer);
-    const userInfoReducer = useSelector((state) => state.userInfoReducer);
-    const wagonsReducer = useSelector((state) => state.wagonsReducer);
-
-
-    const onBack = () => {
+class AdminWagonsByTransportationAdd extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wagons: [],
+            visibleHeader: 'hidden'
+        }
+    }
+    onBack = () => {
         // props.onClose();\
-        props.onClose();
+        this.props.onClose();
     }
-    const onCancel = () => {
-        props.onClose();
+    onCancel = () => {
+        this.props.onClose();
     }
-    const saveChanges = () => {
-        // const name = this.state.name;
-        // const postObj = {
-        //     'name': name
-        // }
-        // this.props.save(postObj);
-        // this.props.onClose();
+    showAddVagonModel = () => {
+        console.log('Wagons state is:' + JSON.stringify(this.state.wagons))
+    }
+    discardChanges = () => {
+        const originalWagons = this.props.wagonsReducer.wagons
+        this.setState({
+            wagons: originalWagons
+        }, () => {
+            this.setState({
+                visibleHeader: 'hidden'
+            })
+        });
+    }
+    saveChanges = () => {
     }
 
-    useEffect(() => {
-        console.log('Transportation id:' + props.match.params.id)
-        setTransportationId(props.match.params.id);
-        if (usersReducer.currentUser !== null) {
-            dispatch(getUserData(1, () => {
-                if (userInfoReducer.role === 'Administrator') {
-                    console.log('Admin is logged')
-                    dispatch(getWagonsByTransportation(props.match.params.id,()=>{
-                        const wagonsClone = JSON.parse(JSON.stringify(wagonsReducer.wagons))
-                        setWagons(wagonsClone);
-                    }));
-                } else {
-                    history.push('/')
+
+    arrayEqual = (array1, array2) => {
+        let a = JSON.parse(JSON.stringify(array1));
+        let b = JSON.parse(JSON.stringify(array2));
+
+        let original = array1;
+        let modified = array2;
+
+        if (a === b) return true;
+        if (a == null || b == null) return false;
+        if (a.length !== b.length) return false;
+
+        a = a.sort();
+        b = b.sort();
+
+        for (var i = 0; i < original.length; i++) {
+            if (original[i].numberOfWagon !== modified[i].numberOfWagon ||
+                original[i].typeOfWagon !== modified[i].typeOfWagon ||
+                original[i].wagonsCount !== modified[i].wagonsCount ||
+                original[i].liftingCapacityTons !== modified[i].liftingCapacityTons ||
+                original[i].weight !== modified[i].weight
+            ) {
+                console.log('They are not equal!!!')
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //check if original companies state and modified are equal or not
+    getUpdateWindowState = () => {
+        // make clones first. i dont want to make any action to them directly
+        const originalWagons = JSON.parse(JSON.stringify(this.props.wagonsReducer.wagons));
+        const modifiedWagons = JSON.parse(JSON.stringify(this.state.wagons));
+
+        if (originalWagons === null) {
+            return 'hidden';
+        }
+        if (modifiedWagons === null) {
+            return 'hidden';
+        }
+        if (this.arrayEqual(originalWagons, modifiedWagons) === false) {
+            return 'visible';
+        }
+        return 'hidden'
+
+    }
+    onDataChange = (value, record, inputName) => {
+        console.log('Value that want to change:'+value)
+        //cloning wagons state to not work directly with it
+        const wagonsClone = JSON.parse(JSON.stringify(this.state.wagons));
+        wagonsClone.map((element, index) => {
+            if (element.id === record.id) {
+                if (inputName === "numberOfWagon") {
+                    element.numberOfWagon = Number(value);
+                } else if (inputName === "typeOfWagon") {
+                    element.typeOfWagon = value;
+                } else if (inputName === "liftingCapacityTons") {
+                    element.liftingCapacityTons = Number(value);
+                } else if (inputName === 'weight') {
+                    element.weight = Number(value);
                 }
-            }));
+            }
+        });
+        // console.log('Modified array is:' + JSON.stringify(wagonsClone));
+        this.setState({
+            wagons: wagonsClone
+        },() => {
+            console.log('Modified array is:'+JSON.stringify(this.state.wagons))
+            const visibilityString = this.getUpdateWindowState();
+            this.setState({
+                visibleHeader: visibilityString
+            });
+        })
+        // this.setState({
+        //     wagons: wagonsClone
+        // }, () => {
+        //     console.log('Original array: ' + JSON.stringify(this.props.wagonsReducer.wagons));
+        //     console.log('Modified array:' + JSON.stringify(this.state.wagons));
+        //     // const visibilityString = this.getUpdateWindowState();
+        //     // this.setState({
+        //     //     visibleHeader: visibilityString
+        //     // });
+        // })
+    }
+
+
+    componentDidMount() {
+        if (this.props.usersReducer.currentUser !== null) {
+            this.props.getUserData(1, () => {
+                if (this.props.userInfoReducer.role === 'Administrator') {
+                    console.log('Admin is logged')
+                    this.props.getWagonsByTransportation(this.props.match.params.id, () => {
+                        const wagonsClone = JSON.parse(JSON.stringify(this.props.wagonsReducer.wagons));
+                        this.setState({ wagons: wagonsClone }, () => {
+                            console.log('Wagons setted to:' + this.state.wagons)
+                        })
+                    });
+                } else {
+                    this.props.history.push('/')
+                }
+            });
 
         } else {
-            history.push('/')
+            this.props.history.push('/')
         }
-    }, [history, dispatch, props.match.params.id,usersReducer.currentUser, userInfoReducer.role]);
-    const columns = [
-        {
-            title: 'Vagono numeris',
-            dataIndex: 'numberOfWagon',
-            width: '25%',
-            render: (text, record, index) => (
-                <Input
-                    tyoe={'text'}
-                    defaultValue={text}
+    }
+    render() {
+        const columns = [
+            {
+                title: 'Vagono numeris',
+                dataIndex: 'numberOfWagon',
+                width: '25%',
+                render: (text, record, index) => (
+                    <InputNumber
+                        tyoe={'text'}
+                        defaultValue={text}
+                        value={text}
+                        onChange={(e) => this.onDataChange(e, record, "numberOfWagon")}
+                    />
+                )
+
+            },
+            {
+                title: 'Vagono tipas',
+                dataIndex: 'typeOfWagon',
+                width: '20%',
+                render: (text, record, index) => (
+                    <Input
+                        type={'text'}
+                        defaultValue={text}
+                        value={text}
+                        onChange={(e) => this.onDataChange(e.target.value,record,"typeOfWagon")}
+                        />
+                )
+            },
+            {
+                title: 'Keliamoji galia(tonomis)',
+                dataIndex: 'liftingCapacityTons',
+                width: '25%',
+                render: (text, record, index) => (
+                    <InputNumber
+                        tyoe={'text'}
+                        defaultValue={text}
+                        value={text}
+                        onChange={(e) => this.onDataChange(e, record, "liftingCapacityTons")}
+                    />
+                )
+            },
+            {
+                title: 'Svoris(tonomis)',
+                dataIndex: 'weight',
+                width: '30%',
+                render: (text, record, index) => (
+                    <InputNumber
+                        tyoe={'text'}
+                        defaultValue={text}
+                        value={text}
+                        onChange={(e) => this.onDataChange(e, record, "weight")}
+                    />
+                )
+            },
+        ]
+        return (
+            <>
+                <UnsavedChangesHeader
+                    visibility={this.state.visibleHeader}
+                    discardChanges={this.discardChanges}
+                    saveChanges={this.saveChanges}
                 />
-            )
-        },
-        {
-            title: 'Vagono tipas',
-            dataIndex: 'typeOfWagon',
-            width: '20%',
-            render: (text, record, index) => (
-                <Input
-                    type={"text"}
-                    defaultValue={text}
-                // onChange={(e) => this.onDataChange(e.target.value, record)}
-                // value={text}
-                // onChange={e => this.onFixedChange(e.target.value, record, "price")}
-                />
-            )
-        },
-        {
-            title: 'Keliamoji galia(tonomis)',
-            dataIndex: 'liftingCapacityTons',
-            width: '25%',
-            render: (text, record, index) => (
-                <Input
-                    tyoe={'text'}
-                    defaultValue={text}
-                />
-            )
-        },
-        {
-            title: 'Svoris(tonomis)',
-            dataIndex: 'weight',
-            width: '30%',
-            render: (text, record, index) => (
-                <Input
-                    tyoe={'text'}
-                    defaultValue={text}
-                />
-            )
-        },
-    ]
-    return (
-        <>
-           <div style={{ marginTop: 45, marginBottom: 45 }}>
+                <div style={{ marginTop: 45, marginBottom: 45 }}>
                     <Col span={24} offset={3}>
-                    <Row gutter={16}>
+                        <Row gutter={16}>
                             <Col span={16}>
                                 <div style={{ marginRight: '40px' }}>
                                     <Typography.Title style={{ ...aboutTitleTextStyle }}>Transportacijos vagonai</Typography.Title>
@@ -147,17 +251,26 @@ function AdminWagonsByTransportationAdd(props) {
                                     <Table
                                         rowKey="id"
                                         columns={columns}
-                                        dataSource={wagons}
+                                        dataSource={this.state.wagons}
                                         pagination={true}
                                     // footer={() => (<Space style={{ display: 'flex', justifyContent: 'space-between' }}><Button size="large" style={{ ...buttonStyle }} onClick={this.onOpenAddCompany()}><PlusOutlined />Pridėti kompaniją</Button></Space>)}
                                     />
-                                    {/* <Space style={{ display: 'flex', justifyContent: 'space-between' }}><Button size="large" style={{ ...buttonStyle }} onClick={this.showAddCompanyModel}><PlusOutlined />Pridėti kompaniją</Button></Space> */}
+                                    {/* <Space style={{ display: 'flex', justifyContent: 'space-between' }}><Button size="large" style={{ ...buttonStyle }} onClick={showAddVagonModel}><PlusOutlined />Pridėti kompaniją</Button></Space> */}
                                 </Card>
                             </Col>
                         </Row>
                     </Col>
                 </div>
-        </>
-    )
+            </>
+        )
+    }
 }
-export default AdminWagonsByTransportationAdd;
+
+const mapStateToProps = (state) => {
+    return {
+        usersReducer: state.usersReducer,
+        userInfoReducer: state.userInfoReducer,
+        wagonsReducer: state.wagonsReducer
+    }
+}
+export default connect(mapStateToProps, { getUserData, getWagonsByTransportation })(withRouter(AdminWagonsByTransportationAdd));
