825b2c73ee653eaf97796de25b66f53c9d11b248
songulija <65647664+songulija@users.noreply.github.com>
2021-11-08 01:12:01 +0200

    Added missing fields to RegisterScreen

--- src/Screens/RegisterScreen.js
+++ src/Screens/RegisterScreen.js
@@ -1,88 +1,94 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
+import { Select } from 'antd'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../styles/Login.css";
-import { register, getUserData } from '../redux/actions/userActions.js'
+import { register } from '../redux/actions/userActions.js';
+import { getCompanies } from '../redux/actions/companiesActions.js'
 import { Link } from 'react-router-dom'
+
+const { Option } = Select;
 
 function RegisterScreen({ location, history }) {
     const dispatch = useDispatch();
-    const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [company, setCompany] = useState('')
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    // const [companies,setCompanies] = useState([])
 
-    const usersReducer = useSelector((state)=> state.usersReducer);
+    const usersReducer = useSelector((state) => state.usersReducer);
+    const userInfoReducer = useSelector((state) => state.userInfoReducer)
+    const companiesReducer = useSelector((state) => state.companiesReducer)
+
     const redirect = location.search ? location.search.split('=')[1] : '/';
-    const submitHandler = ()=>{
+    const submitHandler = () => {
 
     }
 
-    useEffect(()=>{
-        console.log('Register screen ')
-        if(usersReducer.currentUser !== null){
-
-        }else{
+    const changeCompany = (e) => {
+        console.log('Company' + e)
+    }
+    //get companies. becouse when register we asign companies
+    useEffect(() => {
+        if (usersReducer.currentUser !== null && userInfoReducer.role === 'Administrator') {
+            dispatch(getCompanies(1, () => {
+                console.log('Companies:' + JSON.stringify(companiesReducer.companies))
+            }));
+        } else {
             history.push('/')
         }
-    },[dispatch, history,usersReducer.currentUser])
+    }, [dispatch, history, usersReducer.currentUser, userInfoReducer.role])
     return (
         <div className="Login my-auto container-fluid vh-100 vw-100">
             <Form onSubmit={submitHandler}>
                 <h1 className="h3 mb-3 fw-normal">Naudotojų registracija</h1>
                 <Form.Group controlId='text'>
-                    <Form.Label>Name</Form.Label>
+                    <Form.Label>Telefono numeris</Form.Label>
                     <Form.Control
-                        type='name'
-                        placeholder='Enter name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        type='phone'
+                        placeholder='Įveskite telefono numerį'
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
                     >
+
                     </Form.Control>
                 </Form.Group>
-
+                <Form.Group controlId='company'>
+                <Form.Label>Kompanija</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Select>
+                </Form.Group>
                 <Form.Group controlId='email'>
                     <Form.Label>Email</Form.Label>
                     <Form.Control
                         type='email'
-                        placeholder='Enter email'
+                        placeholder='Įveskite el. paštą'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     >
                     </Form.Control>
                 </Form.Group>
 
-                <Form.Group controlId='Password'>
+                <Form.Group controlId='password'>
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                         type='password'
-                        placeholder='Enter password'
+                        placeholder='Įveskite slaptažodį'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     >
-
                     </Form.Control>
-
                 </Form.Group>
 
-
-                <Form.Group controlId='Password'>
-                    <Form.Label>Confirm password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Confirm password'
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    >
-
-                    </Form.Control>
-
-                </Form.Group>
-
-                <Button type='Submit' variant='primary'> Sign In</Button>
+                <Button type='Submit' variant='primary'> Registruoti</Button>
 
             </Form>
         </div>
