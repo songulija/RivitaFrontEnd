9a0ac57b027794e3e9c105095f44031198e82e33
songulija <65647664+songulija@users.noreply.github.com>
2021-11-07 15:07:05 +0200

    Addded updateWagons action and reducer

--- src/redux/reducers/wagonsReducer.js
+++ src/redux/reducers/wagonsReducer.js
@@ -8,6 +8,23 @@ export const wagonsReducer = (state = { wagons: [] }, action) => {
             return {...state,'loading':true}
         case 'TRANSPORTATION_WAGONS_FETCH_SUCCESS':
             return {...state,'loading':false, 'wagons':action.payload}
+        case 'WAGONS_UPDATE_REQUEST':
+            return {...state, 'loading':true}
+        case 'WAGONS_UPDATE_SUCCESS':
+            // in updatedItems array will not be all items. only those that i sent to update
+            const updatedItems = action.payload;
+            const wagonsArray = state.wagons;
+            // map through updatedItems(wagons) array through all items. then map through wagons array and check if
+            // there are items with same id. if so then change 
+            updatedItems.map((obj,index)=>{
+                wagonsArray.map((element,index1)=>{
+                    if(element.id === obj.id){
+                        const updatedObj = obj;
+                        element = updatedObj;
+                    }
+                })
+            });
+            return {...state, 'loading':false, 'wagons':wagonsArray}
         case 'ERROR':
             return { ...state,'loading': false, 'error': action.payload }
         default:
